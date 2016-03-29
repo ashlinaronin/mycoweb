9c4b5560c9c6be9bd7f35dca25a2c1e1b5c23e45
ashlinaronin <ashlin.aronin@pollinate.com>
2016-03-29 14:24:38 -0700

    Clean up and simplify functionality due to other extension

--- inject-styles.js
+++ inject-styles.js
@@ -3,43 +3,6 @@
 
 say('ya');
 
-
-
-var bigBro = {
-    canvas: document.querySelector('#selfie-canvas'),
-    context: canvas.getContext('2d'),
-    webcam: null,
-    dataURL: null
-}
-
-
-function takePic(stream) {
-    context.drawImage(webcam, 0, 0);
-    dataURL = canvas.toDataURL('image/png');
-    postImage(dataURL, 'localhost:8000/saveImg');
-}
-
-function postImage(dataURL, apiEndpoint) {
-    // do AJAX call here to send image to server
-}
-
-function initWebcam() {
-    var p = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-    p.then(function(mediaStream) {
-        webcam = document.querySelector('video#webcam'); // haven't made this yet
-        webcam.src = window.URL.createObjectURL(mediaStream);
-        webcam.onloadedmetadata = function(e) {
-            // do something with video
-            takePic(mediaStream);
-        };
-    });
-
-    p.catch(function(err) {
-        console.log(err.name);
-    });
-}
-
-
 function say(whatToSay) {
     var utterance = new SpeechSynthesisUtterance();
     utterance.text = whatToSay;
