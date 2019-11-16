889fbcef8241c0d2855fed86d5f2b80d380bfcea
ashlinaronin <ashlinaronin@gmail.com>
2019-11-15 22:32:20 -0800

    Create a canvas and put it on top of everything else

--- inject-styles.js
+++ inject-styles.js
@@ -1,9 +1,13 @@
-// image By The original uploader was Lex vB at Dutch Wikipedia. - Originally from nl.wikipedia; description page is/was here., CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=2499669
-// replaceImages('https://upload.wikimedia.org/wikipedia/commons/f/f6/Mushroom%27s_roots_%28myc%C3%A9lium%29.jpg');
+drawMycelium();
 
-function replaceImages(newImageSrc) {
-    var imgTags = document.getElementsByTagName('img');
-    for (var i = 0; i < imgTags.length; i++) {
-        imgTags[i].src = newImageSrc;
-    }
+function drawMycelium() {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    context.fillStyle = "red";
+    context.fillRect(0, 0, 800, 600);
+
+    canvas.className = "mycelium-canvas";
+
+    document.body.appendChild(canvas);
 }
