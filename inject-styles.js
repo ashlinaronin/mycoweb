a9ca0142af030b79aebb2e83f9b463bc11b397bc
ashlinaronin <ashlinaronin@gmail.com>
2019-11-15 22:59:47 -0800

    Use code from Andreas Rohner to draw a tree in the background of the page

--- inject-styles.js
+++ inject-styles.js
@@ -1,13 +1,22 @@
 drawMycelium();
 
 function drawMycelium() {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
+  const canvas = document.createElement('canvas');
 
-    context.fillStyle = "red";
-    context.fillRect(0, 0, 800, 600);
+  canvas.width = 800;
+  canvas.height = 600;
 
-    canvas.className = "mycelium-canvas";
+  canvas.className = "mycelium-canvas";
 
-    document.body.appendChild(canvas);
+  debugger;
+
+  fract = new Fractal.HTree(canvas, {x: 0, y: 0}, 14, "black", "rgba(91,0,144,0.3)", true);
+
+  start = {x: canvas.width / 2, y: canvas.height};
+  target = {x: canvas.width / 4, y: canvas.height / 7};
+  var htreeAnim = new Fractal.Animation(fract, start, target);
+
+  htreeAnim.skip(2000);
+
+  document.body.appendChild(canvas);
 }
