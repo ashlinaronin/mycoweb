eef9133ad34d1b509b72dc092b24773c4324c604
ashlinaronin <ashlinaronin@gmail.com>
2019-12-01 19:00:39 -0800

    Just use simplified tree.js

---
+++ tree.js
@@ -0,0 +1,46 @@
+const theta = 0.5;
+
+function branch(context, length) {
+  context.fillStyle = "black";
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(0, -length);
+  context.stroke();
+  context.moveTo(0, -length);
+  context.translate(0, -length);
+
+  // Each branch’s length shrinks by two-thirds.
+  length *= 0.66;
+
+  console.log(length);
+
+  if (length > 2) {
+    // draw left side
+    context.save();
+    context.rotate(theta);
+    // Subsequent calls to branch() include the length argument.
+    branch(context, length);
+    context.restore();
+
+    // draw right side
+    context.save();
+    context.rotate(-theta);
+    branch(context, length);
+    context.restore();
+  }
+}
+
+function init() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 800;
+  canvas.height = 600;
+  canvas.className = "mycelium-canvas";
+
+  const context = canvas.getContext("2d");
+
+  branch(context, 16);
+
+  document.body.appendChild(canvas);
+}
+
+init();
