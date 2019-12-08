a2de453da4b81b93c9e33ca61dbff6bf8ab55cd9
ashlinaronin <ashlinaronin@gmail.com>
2019-12-07 21:20:28 -0800

    doActualPath so we can start to capture lines for animation

--- tree.js
+++ tree.js
@@ -1,20 +1,21 @@
 const theta = 0.65;
+const paths = [
+  [
+    [400, 600],
+    [0, 0]
+  ]
+];
 
 async function wait(milliseconds) {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-async function branch(context, length) {
+async function branch(context, length, angle = 0) {
   context.strokeStyle = "green";
 
   await wait(50);
 
-  context.beginPath();
-  context.moveTo(0, 0);
-  context.lineTo(0, -length);
-  context.stroke();
-  context.moveTo(0, -length);
-  context.translate(0, -length);
+  doActualPath(context, length, angle);
 
   // Each branch’s length shrinks by two-thirds.
   length *= 0.66;
@@ -26,15 +27,40 @@ async function branch(context, length) {
     context.save();
     context.rotate(theta);
     // Subsequent calls to branch() include the length argument.
-    await branch(context, length);
+    await branch(context, length, theta);
     context.restore();
 
     // draw right side
     context.save();
     context.rotate(-theta);
-    await branch(context, length);
+    await branch(context, length, -theta);
     context.restore();
   }
+}
+
+function doActualPath(context, length, angle) {
+  debugger;
+
+  const lastPath = paths[paths.length-1];
+
+  const newTo = [
+    lastPath[0][0] + length * Math.cos(angle),
+    lastPath[0][1] + length * Math.sin(angle)
+  ];
+  const newFrom = lastPath[0];
+
+  console.log(`new path from ${newFrom} to ${newTo}`);
+
+  paths.push([newFrom, newTo]);
+
+  console.log(paths);
+
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(0, -length);
+  context.stroke();
+  context.moveTo(0, -length);
+  context.translate(0, -length);
 }
 
 function init() {
